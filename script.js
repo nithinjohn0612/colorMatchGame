94fdd9794aa8676e2c411b47557f39c90e3cdf6d
Nithin John Cherian <Nithinjohn2811"gmail.com>
2025-03-22 18:27:50 +0000

    created the random colour function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+//intial state
 const createIntialState = ()=>({
     targetColour:a_function(),
     playerColour:{r:0,g:0,b:0},
     score:0,
     gameOver:false
 })
+
+//generating a random colour as the target
+const generateRandomColour = () =>{
+    return {
+        r:Math.floor(Math.random() * 256),
+        g:Math.floor(Math.random() * 256),
+        b:Math.floor(Math.random() * 256)
+    }
+}
+console.log(generateRandomColour())
